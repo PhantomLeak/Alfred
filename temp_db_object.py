c95078ca653f5c93fec74c2c7aca8f9e4afaf7da
Dylan Stocking <dstocking0823@gmail.com>
2022-10-30 23:11:47 -0400

    Add db object file

---
+++ temp_db_object.py
@@ -0,0 +1,49 @@
+#TODO: Setup a db to hold question requests, answers, and possible initiators to allow alfred to learn.
+game_initiators = [
+    'i want to play a game',
+    'lets play a game',
+    "let's play a game",
+    'i wanna play a game',
+    'i want to play rock paper scissors',
+    'lets play rock paper scissors',
+    "Let's play rock paper scissors",
+    'lets play the guessing game',
+    "Let's play the guessing game",
+    'lets play guessing game',
+    "Let's play guessing game",
+]
+password_initiators = [
+    'i need a password',
+    'i need to make a password',
+    'make a password',
+    'create a password',
+    'make me a password'
+]
+weather_initiators = [
+    'tell me the weather',
+    'what is the weather gonna be like',
+    'how will the weather be today',
+    'give me the forcast for today',
+    'tell me the weather forcast',
+    'weather forecast'
+]
+
+joke_initiators = [
+    'joke',
+    'tell me a joke',
+    'can you tell me a joke',
+    'can you tell me a joke?',
+    'i want to hear something funny'
+]
+
+local_storage_iniators = [
+    'clear local storage',
+    'clear cache',
+    'clean computer'
+]
+
+conversation = [
+    'how are you',
+    'how are you doing today',
+    'i wanna talk'
+]
