321b920c6cd96b52550a4e8fe7ba2cbfb30f7517
Phantomleak <dstocking0823@gmail.com>
2022-11-26 07:29:50 -0500

    Start comparing both datetimes to give ability to set a timer for a specific time (9pm for example)

--- src/functions/reminders.py
+++ src/functions/reminders.py
@@ -1,6 +1,7 @@
 import chime
 from threading import Timer
 from functions.terminal_colors import colors as terminal_message
+from datetime import datetime
 
 tc = terminal_message()
 
@@ -14,8 +15,8 @@ def set_reminder(reminder):
     
     reminder_time = set_reminder_time(float(time_digit), after_keyword)
     reminder_message = get_reminder_message(before_keyword, after_keyword)
-
-    Timer(reminder_time, alert, args=[reminder_message]).start()
+    
+    # Timer(reminder_time, alert, args=[reminder_message]).start()
 
 
 # Set the specified time the user wishes to be reminded in 
@@ -26,6 +27,22 @@ def set_reminder_time(time, time_span):
         return ((time * 60) * 60)
     elif 'days' in time_span or 'day' in time_span:
         return (time * 86400) # There are 86400 seconds in a day
+    elif 'am' in time_span or 'pm' in time_span: # If the user specifies a time to set the reminder at
+        if 'am' in time_span:
+            time_def = 'AM'
+        if 'pm' in time_span:
+            time_def = 'PM'
+
+        # Get / Set requested user time in hour:minute format
+        requested_time = datetime.strptime(f'{int(time)} {time_def}', "%I %p")
+        # requested_time_formatted = datetime.strftime(requested_time, "%H:%M")
+        
+        # Get / Set the current time in hour:minute format
+        current_time = datetime.now()
+        # current_time_formatted = datetime.strftime(current_time, "%H:%M")
+
+        time_diff = (requested_time - current_time)
+        print(time_diff)
 
 
 # Get the message that is being reminded to the user
@@ -53,12 +70,9 @@ def get_reminder_message(before, after):
         return_message = after_keyword
     
     return return_message
-    
+
 
 # Chime and display alert once the specified timespan has finished
 def alert(reminder_message):
     chime.info()
     print(tc.reminder_message(reminder_message))
-
-
-
