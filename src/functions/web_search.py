8bd8ebbc56482b9e89644a9283cd622e41047ebf
Phantomleak <dstocking0823@gmail.com>
2023-01-22 16:21:44 -0500

    Add Comments to web browser code

--- src/functions/web_search.py
+++ src/functions/web_search.py
@@ -13,6 +13,7 @@ def search_web(search_request: str = None):
 
     return success_message
 
+
 # Take user search string and return a working URL
 def create_search_url(url: str = None):
     search_site = ''
@@ -31,17 +32,17 @@ def create_search_url(url: str = None):
 
         search_site = f"https://www.google.com/search?q={quote(search_site)}"
 
-
     return search_site
+
 
 def split_string_url(url: str = None, split_itr: str = None):
     split = url.split(split_itr)
 
     url_string = split[1]
 
-    if split_itr == 'open':
+    if split_itr == 'open':  # Only remove whitespace if returning a URL link
         formatted_url_string = "".join(url_string.split())
-    else: # this would be used for a normal google search rather than a URL
+    else:  # this would be used for a normal google search rather than a URL
         formatted_url_string = url_string
 
     return formatted_url_string
