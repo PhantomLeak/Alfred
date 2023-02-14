83053700daea92e5b3497af77297672949867380
Phantomleak <dstocking0823@gmail.com>
2023-02-13 22:21:11 -0500

    Clean up weather outputs to be in imperal format, remove library call that was returning odd results and use request instead

--- src/functions/weather.py
+++ src/functions/weather.py
@@ -1,7 +1,7 @@
-from pyowm.owm import OWM
 from geopy.geocoders import Nominatim
+import requests
+import json
 from src.api_keys import OPEN_WEATHER_API_KEY
-import re
 
 # Initialize Nominatim API
 geolocator = Nominatim(user_agent="MyApp")
@@ -9,32 +9,34 @@ geolocator = Nominatim(user_agent="MyApp")
 
 def weather(weather_init: str):
     location = get_weather_location(weather_init)
-
-    owm = OWM(OPEN_WEATHER_API_KEY)
     lon = location.longitude
     lat = location.latitude
 
-    mgr = owm.weather_manager()
-    observation = mgr.weather_at_coords(lon, lat)
-    w = observation.weather
+    data = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={OPEN_WEATHER_API_KEY}&units=imperial')
+    data_return = json.loads(data.text)
 
-    temp = w.temperature()
-    wind = w.wind()
+    temp = data_return.get('main')
+    wind = data_return.get('wind')
+    overcast = data_return.get('weather')
+
     weather_return = ''
-    weather_return += (f"- The overcast today is going to be {w.detailed_status} \n")
+    weather_return += (f"- The weather today is going to be {overcast[0]['description']} \n")
     weather_return += '\n'
-    weather_return += (
-        f"- With wind speeds of {wind['speed']} MPH, at {wind['deg']} degrees with gusts up to {wind['gust']} MPH \n")
+    weather_return += (f"- With wind speeds of {wind['speed']} MPH, at {wind['deg']} degrees \n")
     weather_return += '\n'
-    weather_return += (
-        f"- The temperature for today will be {temp['temp']} with the high being {temp['temp_max']} overall it will feel like {temp['feels_like']} \n")
+    weather_return += f"- The temperature today will reach a high of {temp['temp_max']} and a low of {temp['temp_min']}."
+    weather_return += f"Currently, the temperature is {temp['temp']} with {temp['humidity']}% humidity\n"
 
     return weather_return
 
 
 def get_weather_location(weather_init: str):
-    if 'in' in weather_init:
-        user_location = weather_init.split('in')[1]
+    word_split = 'in'
+    if 'for' in weather_init:
+        word_split = 'for'
+
+    if word_split in weather_init:
+        user_location = weather_init.split(word_split)[1]
         location = "".join(user_location)
     else:
         location = input("Please Enter the city name and state ('Orlando, FL): ")
