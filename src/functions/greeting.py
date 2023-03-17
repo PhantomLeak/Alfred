287264acaf62748a45e6c21a85483fde003600cf
Phantomleak <dstocking0823@gmail.com>
2023-03-16 22:51:10 -0400

    Update greeting and add to commit

---
+++ src/functions/greeting.py
@@ -0,0 +1,7 @@
+import calendar  # Converts daytime number into day of the week
+from datetime import datetime  # Used to get current day of the week
+
+
+def greeting():
+    today = calendar.day_name[datetime.today().weekday()]
+    return f'Hello Sir, how can I help you on this fine {today}?'
